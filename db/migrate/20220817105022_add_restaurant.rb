87bc54a4c8c2c4b56f6eda1229a7c85384db131f
daniel dantas <danieldantas0@hotmail.com>
2022-08-17 15:40:01 +0100

    exerceise

---
+++ db/migrate/20220817105022_add_restaurant.rb
@@ -0,0 +1,5 @@
+class AddRestaurant < ActiveRecord::Migration[7.0]
+  def change
+    
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Wiping seeds"
+Restaurant.destroy_all
+
+puts "Creating seeds"
+
+restaurant_one = Restaurant.create(name: "McDonalds", address: "Hoxton Street", phone_number: "01234 578990", category: "chinese")
+restaurant_two = Restaurant.create(name: "Munchie", address: "Hoxton TwoStreet", phone_number: "01234 840200", category: "italian")
+restaurant_three = Restaurant.create(name: "Casa Guedes", address: "Hoxton ThreeStreet", phone_number: "01234 283648", category: "japanese")
+restaurant_four = Restaurant.create(name: "Braganças", address: "Hoxton FourStreet", phone_number: "01234 627394", category: "french")
+restaurant_five = Restaurant.create(name: "DaTerra", address: "Hoxton FiveStreet", phone_number: "01234 987123", category: "belgian")
+
+puts "Seeds created!"
